32877862e5487d5f82ab4b654ba814c524d46cb8
Mark <vakulenko.mark3@gmail.com>
2020-04-19 05:10:01 +0300

    var and import

--- mainb.py
+++ mainb.py
@@ -1,2 +1,13 @@
-import math
-print("JAVA")
+import numpy as np
+import mathplotlib.pyplot as plt
+
+
+pmin, pmax, qmin, qmax = -2.5, 1.5, -2, 2
+
+ppoints, qpoints = 200, 200
+
+max_iterations = 300
+
+infinity_border = 10
+
+image = np.zeros((ppoints, qpoints))
